efa1488a5c25d4ee7d6ace5507654b96cf73371e
jumaniyozov <jumaniyozovse@gmail.com>
2021-09-21 06:47:07 +0500

    fixed & polished, prepublish

--- src/components/QuestionBox.component.jsx
+++ src/components/QuestionBox.component.jsx
@@ -1,122 +1,127 @@
-import React, { useEffect, useState } from "react";
-import { questions } from "../data/tempData";
-import { shuffle } from "../helpers/shuffler";
-import { useDispatch, useSelector } from "react-redux";
-import { bindActionCreators } from "redux";
-import { actionCreators } from "../store";
+import React, {useEffect, useState} from "react";
+import {questions} from "../data/tempData";
+import {shuffle} from "../helpers/shuffler";
+import {useDispatch, useSelector} from "react-redux";
+import {bindActionCreators} from "redux";
+import {actionCreators} from "../store";
 
 export default function MainBox(props) {
     const userState = useSelector((state) => state.user);
     const dispatch = useDispatch();
     const AC = bindActionCreators(actionCreators, dispatch);
 
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+    const [score, setScore] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const [questionList, setQuestionList] = useState([]);
+    const [currSec, setCurrSec] = useState(0);
+    const [currMin, setCurrMin] = useState(0);
 
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [showScore, setShowScore] = useState(false);
-  const [score, setScore] = useState(0);
-  const [loading, setLoading] = useState(true);
-  const [questionList, setQuestionList] = useState([]);
-  const [currSec, setCurrSec] = useState(0);
-  const [currMin, setCurrMin] = useState(0);
+    const handleAnswerOptionClick = (isCorrect) => {
+        if (isCorrect) {
+            setScore(score + 1);
+        }
 
-
-  const handleAnswerOptionClick = (isCorrect) => {
-    if (isCorrect) {
-      setScore(score + 1);
-    }
-
-    const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < questionList.length) {
-      setCurrentQuestion(nextQuestion);
-    } else {
-      setShowScore(true);
-    }
-  };
-
-  const handleClick = () => {
-    AC.updateUserList({name: userState.name, score: score, time: `${currMin}:${currSec}`});
-    props.history.push("/")
-  }
-
-  
-
-  useEffect( () => {
-    setInterval(() => { setCurrSec(prevState => (prevState + 1));}, 1000);
-    }, []);
-    
-  useEffect( () => {
-     console.log(currSec)
-    if(currSec === 60) {
-      setCurrMin(prevState => (prevState + 1));
-      setCurrSec(0)
-    }
-  }, [currSec]);
-
-  useEffect(() => {
-    const get5RandomQuestions = async () => {
-      const shuffled = shuffle(questions);
-      return shuffled.slice(0, 5);
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questionList.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+        }
     };
 
-    get5RandomQuestions().then((data) => {
-      setQuestionList(data);
-    });
-  }, []);
+    const handleClick = () => {
+        AC.updateUserList({
+            name: userState.name,
+            score: score,
+            time: `${currMin < 10 ? `0${currMin}` : `${currMin}`}:${currSec < 10 ? `0${currSec}` : `${currSec}`}`,
+        });
+        props.history.push("/");
+    };
 
-  useEffect(() => {
-    if (questionList.length >= 5) {
-      setLoading(false);
-    }
-  }, [questionList]);
+    useEffect(() => {
+        setInterval(() => {
+            setCurrSec((prevState) => prevState + 1);
+        }, 1000);
+    }, []);
 
-  return (
-    <div className="app">
-      {loading ? (
-        <>
-          <h2>Загрузка...</h2>
-        </>
-      ) : (
-        <>
-          {showScore ? (
-            <div className="score-section">
-              Вы ответили правильно на {score} вопросов из {questionList.length}
-              {/* <Link to="/">Home</Link> */}
-              <button onClick={handleClick}>На главную страницу</button>
-            </div>
-          ) : (
-            <>
-              <div class="wrapper">
-                <p>
-                  <span id="seconds">{`${currMin}`}</span>:<span id="tens">{`${currSec}`}</span>
-                </p>
-              </div>
-              <div className="question-section">
-                <div className="question-count">
-                  <span>Вопрос {currentQuestion + 1}</span>/
-                  {questionList.length}
-                </div>
-                <div className="question-text">
-                  {questionList[currentQuestion].questionText}
-                </div>
-              </div>
-              <div className="answer-section">
-                {questionList[currentQuestion].answerOptions.map(
-                  (answerOption, index) => (
-                    <button
-                      key={index}
-                      onClick={() =>
-                        handleAnswerOptionClick(answerOption.isCorrect)
-                      }
-                    >
-                      {answerOption.answerText}
-                    </button>
-                  )
-                )}
-              </div>
-            </>
-          )}
-        </>
-      )}
-    </div>
-  );
+    useEffect(() => {
+        console.log(currSec);
+        if (currSec === 60) {
+            setCurrMin((prevState) => prevState + 1);
+            setCurrSec(0);
+        }
+    }, [currSec]);
+
+    useEffect(() => {
+        const get5RandomQuestions = async () => {
+            const shuffled = shuffle(questions);
+            return shuffled.slice(0, 5);
+        };
+
+        get5RandomQuestions().then((data) => {
+            setQuestionList(data);
+        });
+    }, []);
+
+    useEffect(() => {
+        if (questionList.length >= 5) {
+            setLoading(false);
+        }
+    }, [questionList]);
+
+    return (
+        <div className="question--box">
+            {loading ? (
+                <>
+                    <h2>Загрузка...</h2>
+                </>
+            ) : (
+                <>
+                    {showScore ? (
+                        <div className="score-section">
+                            Вы ответили правильно на {score} вопросов из {questionList.length}
+                            {/* <Link to="/">Home</Link> */}
+                            <button onClick={handleClick}>На главную страницу</button>
+                        </div>
+                    ) : (
+                        <>
+                            <div className="question-section">
+                                <div className="question--text__section">
+                                    <div className="question-count">
+                                        <span>Вопрос {currentQuestion + 1}</span>/
+                                        {questionList.length}
+                                    </div>
+                                    <div className="question-text">
+                                        {questionList[currentQuestion].questionText}
+                                    </div>
+                                </div>
+                                <div className="question--time__section">
+                                    <p>
+                                        <span id="seconds">{`${currMin}`}</span>:
+                                        <span id="tens">{`${currSec}`}</span>
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="answer-section">
+                                {questionList[currentQuestion].answerOptions.map(
+                                    (answerOption, index) => (
+                                        <button
+                                            key={index}
+                                            onClick={() =>
+                                                handleAnswerOptionClick(answerOption.isCorrect)
+                                            }
+                                        >
+                                            {answerOption.answerText}
+                                        </button>
+                                    )
+                                )}
+                            </div>
+                        </>
+                    )}
+                </>
+            )}
+        </div>
+    );
 }
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,74 +1,79 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { bindActionCreators } from "redux";
-import { actionCreators } from "../store";
+import React, {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {bindActionCreators} from "redux";
+import {actionCreators} from "../store";
 
 export default function HomePage(props) {
-  const state = useSelector((state) => state);
-  const dispatch = useDispatch();
-  const AC = bindActionCreators(actionCreators, dispatch);
-  const [userName, setUserName] = useState("");
+    const state = useSelector((state) => state);
+    const dispatch = useDispatch();
+    const AC = bindActionCreators(actionCreators, dispatch);
+    const [userName, setUserName] = useState("");
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    if (userName.length > 0 && userName.trim() !== "") {
-      AC.setUserName(userName);
-      props.history.push("/questions");
-    }
-  };
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        if (userName.length > 0 && userName.trim() !== "") {
+            AC.setUserName(userName);
+            props.history.push("/questions");
+        }
+    };
 
-  return (
-    <>
-      <div className="app2">
-        <h3>Таблица лидеров</h3>
-        <div>
-          <table>
-            <tr>
-              <th>№</th>
-              <th>Имя</th>
-              <th>Очки</th>
-              <th>Время</th>
-            </tr>
-            {state.user.userList
-              .sort((first, second) => {
-                if (first.score > second.score) {
-                  return -1;
-                }
-                if (first.score < second.score) {
-                  return 1;
-                }
-                return 0;
-              })
-              .slice(0, 5)
-              .map((user, index) => {
-                return (
-                  <tr>
-                    <th>{`${index + 1}`}</th>
-                    <th>{`${user.name}`}</th>
-                    <th>{`${user.score}`}</th>
-                    <th>{`${user.time}`}</th>
-                  </tr>
-                );
-              })}
-          </table>
-          <ol></ol>
-        </div>
-      </div>
+    return (
+        <>
+            <div className="app--secondary">
+                <h1>Таблица лидеров</h1>
+                <div>
+                    <table>
+                        <tr>
+                            <th>№</th>
+                            <th>Имя</th>
+                            <th>Очки</th>
+                            <th>Время</th>
+                        </tr>
+                        {state.user.userList
+                            .sort((first, second) => {
+                                if (first.score > second.score) {
+                                    return -1;
+                                }
+                                if (first.score < second.score) {
+                                    return 1;
+                                }
+                                return 0;
+                            })
+                            .slice(0, 5)
+                            .map((user, index) => {
+                                return (
+                                    <tr>
+                                        <td>{`${index + 1}`}</td>
+                                        <td>{`${user.name}`}</td>
+                                        <td>{`${user.score}`}</td>
+                                        <td>{`${user.time}`}</td>
+                                    </tr>
+                                );
+                            })}
+                    </table>
+                    <ol></ol>
+                </div>
+            </div>
 
-      <div className="app3">
-        <h3>Пожалуйста введите ваше имя</h3>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Имя Пользователя:
-            <input
-              type="text"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    </>
-  );
+            <div className="app">
+                <div className="app--header__section">
+                    <h3>Пожалуйста введите ваше имя</h3>
+                </div>
+
+                <div className="app--form__section">
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            <p className="user--text__name">Имя Пользователя:</p>
+                            <input
+                                type="text"
+                                value={userName}
+                                onChange={(e) => setUserName(e.target.value)}
+                            />
+                        </label>
+                        <button type="submit">Начать</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
 }
